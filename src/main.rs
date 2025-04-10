aecd3469f3c74fe051379d9be27b4f9c1c96b63d
Rasul Fazlyev <CuteGirlsPanties@yandex.ru>
2025-04-11 01:07:59 +0500

    test arch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    #[cfg(target_os = "linux")]
+    print!("Hello, Linux ");
+    #[cfg(target_os = "macos")]
+    print!("Hello, Macos ");
+    #[cfg(target_os = "windows")]
+    print!("Hello, Windows ");
+    
+    #[cfg(target_arch = "x86_64")]
+    println!("x86_64!");
+    #[cfg(target_arch = "aarch64")]
+    println!("aarch64!");
 }
